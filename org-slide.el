e2df26e37c686513985a3c2281654f5f690863ef
da <>
2024-06-07 06:24:43 +0200

    add alist to track slidecounts instead of :count

--- org-slide.el
+++ org-slide.el
@@ -26,22 +26,27 @@
 ;;; Code:
 
 (require 'org)
+(defvar org-slide-alist '() "")
+(make-variable-buffer-local 'org-slide-alist)
 (defun org-dblock-write:org-slide (params)
   "Show the next slide in an org-slide dynamic block.
 PARAMS contains the text of the block and everything else passed by the user."
   (let* ((text      (substring (plist-get params :content) 0 -1)) ; remove last newline from text
-         (count     (plist-get params :count)) ; TODO check for nil, don't break when missing
          ; NOTE: we could allow custom delimiters here
          ;(delimiter (or (plist-get params :delimiter) "# SLIDE"))
          (delimiter "# SLIDE")
          ;(indent (plist-get params :indentation-column)) ; TODO account for indentation
          (slides '(0))
-         (new-count (1+ count))
+         (id     (plist-get params :id))
          (pos-of-block-in-buf (point))
          last-match
          (end-of-block-in-buf (+ pos-of-block-in-buf (length text))))
     (setq delimiter (regexp-quote delimiter)) ; escape special regex chars just in case
     (save-excursion (insert text))
+
+    (setq count (alist-get id org-slide-alist -1 nil 'string-equal))
+    (when (eq count -1) (progn (push (cons id "0") org-slide-alist) (setq count 0)))
+    (setf new-count (1+ count))
 
     (setq last-match (string-match delimiter text))
     (while (not (equal last-match nil)) ;; fill slides list with indices
@@ -50,13 +55,6 @@ PARAMS contains the text of the block and everything else passed by the user."
     (setq slides (append slides (list end-of-block-in-buf)))
 
     (when (>= new-count (length slides)) (setq new-count 0)) ; wraparound
-
-    (save-excursion
-      (let ((curr-count (concat ":count " (number-to-string count)))
-            (next-count (concat ":count " (number-to-string new-count))))
-        (forward-line -1)            ; go to #+BEGIN:... line
-        (org-hide-block-toggle 'off) ; make block fully visible in case it's hidden
-        (replace-string-in-region curr-count next-count nil pos-of-block-in-buf)))
 
     ; TODO rewrite
     (outline-flag-region (- pos-of-block-in-buf 1) end-of-block-in-buf t)
@@ -67,14 +65,17 @@ PARAMS contains the text of the block and everything else passed by the user."
         (outline-flag-region (- pos-of-block-in-buf 2) end-of-block-in-buf nil)
         (outline-flag-region hide-from hide-to nil)))
 
+    (setf (alist-get id org-slide-alist -1 nil 'string-equal) new-count)
+
     ; show latex and images in case they were part of the slide
+    (not-modified)
     (org-latex-preview)
     (org-display-inline-images)))
 
 (defun org-slide-insert-dblock ()
   "Create an org-slide dynamic block at point."
   (interactive)
-  (org-create-dblock (list :name "org-slide" :count 0))
+  (org-create-dblock (list :name "org-slide" :id "slideshow-name"))
   (save-excursion
     (forward-line)
     (insert "1st slide...\n# SLIDE\n2nd slide...\n# SLIDE\n3rd slide...")))
